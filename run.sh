9889438f4aee278bde572a4d34f5e8f5d789b3ad
Fir Li <55825587+Fir-lat@users.noreply.github.com>
2024-05-11 21:47:28 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
-python3 llms_collab.py --llm Gemini-1.0-Pro GPT-3.5-Turbo Mixtral-8x7b-Groq --policy simple_test --num_questions 0 2
-python3 llms_collab.py --llm GPT-3.5-Turbo GPT-3.5-Turbo GPT-3.5-Turbo --policy debate_test --num_questions 0 2
-python3 llms_collab.py --llm GPT-3.5-Turbo GPT-3.5-Turbo GPT-3.5-Turbo --policy chain_test --num_questions 0 2
+python3 llms_collab.py --llm Gemini-1.0-Pro GPT-3.5-Turbo Mixtral-8x7b-Groq Qwen-72b-Chat --policy simple_test --num_questions 0 1319
+python3 llms_collab.py --llm Gemini-1.0-Pro Gemini-1.0-Pro Gemini-1.0-Pro --policy debate_test --num_questions 0 1319
+python3 llms_collab.py --llm Gemini-1.0-Pro Gemini-1.0-Pro Gemini-1.0-Pro --policy chain_test --num_questions 0 1319
+
+# python3 llms_collab.py --llm Gemini-1.0-Pro GPT-3.5-Turbo Qwen-72b-Chat --policy debate_test --num_questions 0 1319
+# python3 llms_collab.py --llm Gemini-1.0-Pro GPT-3.5-Turbo Qwen-72b-Chat --policy chain_test --num_questions 0 1319
